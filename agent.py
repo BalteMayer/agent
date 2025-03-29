050a85afeaa398f49aa50386726d4c467f059a3e
BalteMayer <baltemayer@gmail.com>
2025-03-29 14:29:30 +0800

    daul aagency 0.3.0

--- agent.py
+++ agent.py
@@ -4,9 +4,9 @@ import httpx
 from swarm import Swarm, Agent
 import cv2 as cv
 from tools.chart import plot_material_category_distribution, plot_sign_in_trend, plot_group_member_count
-from utils.db_structure import get_db_struct_and_dtype
 from utils.condense import condense_msg
 import json
+import time
 
 
 # API密钥和基础URL设置
@@ -32,8 +32,10 @@ class database:
     pass
 
 
-def query_and_compute(start_index: str, last_index: str, db_info: str):
-    print(start_index, last_index, db_info)
+def query_and_compute(start_index: str, last_index: str, chart_info):
+    with open("config.json", "r", encoding="utf-8") as f:
+        db_info: str = json.dumps(json.load(f), ensure_ascii=False)
+    print(start_index, last_index, chart_info, db_info)
     return f"根据数据库信息{db_info}，查询{start_index}到{last_index}的数据，然后进行统计分析"
 
 def transmit_refined_params_and_db_info(time_info: str, chart_info: str):
@@ -55,7 +57,7 @@ def transmit_refined_params_and_db_info(time_info: str, chart_info: str):
         instructions=
         """
         你是一个数据分析助手，你每次都请务必根据message里的信息调用query_and_compute函数，获取分析结果,
-        query_and_compute(start_index: str, last_index: str, chart_info: str, db_info: str)
+        query_and_compute(start_index: str, last_index: str, chart_info: str,)
         这个函数用于从数据库里获取数据，然后把数据进行一些统计处理，最后返回str类型的分析结果，用于提供给前端绘图。
         你的messages格式是固定的，请注意其中的time_info, chart_info, db_info
         你要根据db_info的信息，，把time_info和chart_info调整为对应的格式，然后把他们作为参数传入query_and_compute函数里
@@ -67,18 +69,22 @@ def transmit_refined_params_and_db_info(time_info: str, chart_info: str):
         time_info是"2025年4月",而根据db_info，其应该是"2025-04"这样的格式，那么
         start_index: str = "2025-04-01", last_index: str = "2025-04-30"
         同理如果chart_info是"考勤情况"，而根据db_info，其应该对应"attendance"这个表单，而你注意到这个表单记录了这个月每位员工的"出勤","迟到","缺勤"情况。那么你可以据此判断应该绘制一个饼状图
-        那么你判断chart_type = "pie"
-        然后把database的信息原封不动传入database: str这个参数。       
+        那么你判断chart_type适合哪一种图然后填入       
         你将得到一个str类型的返回值
         """,
-        Functions=[query_and_compute]
+        functions=[query_and_compute]
     )
     print("OK")
 
+    start_time = time.time()
     assistant = swarm_client.run(
         agent = hazuki,
-        messages = message
+        messages = [{"role": "user", "content": message}]
     )
+    end_time = time.time()
+    print(f"耗时: {end_time - start_time:.2f}秒")
+
+
     print("OK")
     del hazuki
     print("OK")
